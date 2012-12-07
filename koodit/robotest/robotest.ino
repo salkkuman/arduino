fcfcd8c42e1084bbd2777bd0d22ee0fa7925def3
salkkuman <mikko.salama@gmail.com>
2012-12-07 15:23:57 +0200

    robo fiksul ohjauksel ja vaisto

--- koodit/robotest/robotest.ino
+++ koodit/robotest/robotest.ino
@@ -1,14 +1,14 @@
 // # Editor     : Lauren from DFRobot
 // # Date       : 17.02.2012
- 
+
 // # Product name: L298N motor driver module DF-MD v1.3
 // # Product SKU : DRI0002
 // # Version     : 1.0
- 
+
 // # Description:
 // # The sketch for using the motor driver L298N
 // # Run with the PWM mode
- 
+
 // # Connection:
 // #        M1 pin  -> Digital pin 4
 // #        E1 pin  -> Digital pin 5
@@ -21,133 +21,156 @@
 // # Note: You should connect the GND pin from the DF-MD v1.3 to your MCU controller. They should share the GND pins.
 // #
 #include <Ultrasonic.h>
-Ultrasonic ultrasonic(7);
+Ultrasonic ultrasonic(8);
 int E1 = 6;
 int M1 = 7;
 int E2 = 5;                         
 int M2 = 4;
-int sonar = 13;
-signed char x = 125;
-signed char y = 125;
-int vaisto = 0;
 
- 
+signed char x = 0;
+signed char y = 0;
+signed char z = 0;
+int vaisto = 0;
+int ohjausL=0;
+int ohjausR=0;
+int tempx=0;
+int tempy=0;
+
 void setup() 
 { 
-    pinMode(M1, OUTPUT);   
-    pinMode(M2, OUTPUT); 
-    pinMode(A5, INPUT);
-    pinMode(12, OUTPUT);
-    digitalWrite(12,HIGH);
-    Serial1.begin(9600);
+  pinMode(M1, OUTPUT);   
+  pinMode(M2, OUTPUT); 
+  pinMode(A5, INPUT);
+  pinMode(12, OUTPUT);
+  digitalWrite(12,HIGH);
+  Serial1.begin(9600);
+  Serial.begin(9600);
 }
 
-void goRight(){
+void goRight(unsigned int nopeus){
   //digitalWrite(M1,LOW);
-    digitalWrite(M1,HIGH);   
-    digitalWrite(M2,HIGH);       
-    analogWrite(E1, 0);   //PWM Speed Control
-    analogWrite(E2, 255); 
+  digitalWrite(M1,HIGH);   
+  digitalWrite(M2,HIGH);       
+  if(nopeus>255){
+    nopeus=255; 
+  }
+  analogWrite(E1, 0);   //PWM Speed Control
+  analogWrite(E2, nopeus); 
 }
 
-void goLeft(){
+void goLeft(unsigned int nopeus){
   //digitalWrite(M2,LOW);
-    digitalWrite(M1,HIGH);   
-    digitalWrite(M2,HIGH);       
-    analogWrite(E1, 255);   //PWM Speed Control
-    analogWrite(E2, 0); 
+  digitalWrite(M1,HIGH);   
+  digitalWrite(M2,HIGH);       
+  if(nopeus>255){
+    nopeus=255 ;
+  }
+  analogWrite(E1, nopeus);   //PWM Speed Control
+  analogWrite(E2, 0); 
 }
- 
- 
-void goStraight(){
-    digitalWrite(M1,HIGH);   
-    digitalWrite(M2,HIGH);       
-    analogWrite(E1, 255);   //PWM Speed Control
-    analogWrite(E2, 255); 
+
+
+void goStraight(unsigned int nopeus){
+  digitalWrite(M1,HIGH);   
+  digitalWrite(M2,HIGH);       
+  if(nopeus>255){
+    nopeus=255 ;
+  }
+  analogWrite(E1, nopeus);   //PWM Speed Control
+  analogWrite(E2, nopeus); 
 
 }
 
-void goBack(){
-    digitalWrite(M1,LOW);   
-    digitalWrite(M2,LOW);       
-    analogWrite(E1, 255);   //PWM Speed Control
-    analogWrite(E2, 255); 
+void goBack(unsigned int nopeus){
+  digitalWrite(M1,LOW);   
+  digitalWrite(M2,LOW); 
+  if(nopeus>255){
+    nopeus=255 ;
+  }  
+  analogWrite(E1, nopeus);   //PWM Speed Control
+  analogWrite(E2, nopeus); 
 
 }
 
 void loop() 
 { 
   //int val = analogRead(A5);    // read the input pin
- // Serial1.println(val);
-  int vaisto=0;
+  // Serial1.println(val);
+
   if(Serial1.available()>2){
     Serial1.print(Serial1.read());
-    
+
     if(Serial1.read()==0x80){
       x = Serial1.read();
-      y = Serial1.read(); 
+      y = Serial1.read();
+      z = Serial1.read(); 
       Serial1.print(x);
       Serial1.print(y);
+      Serial1.print(z);
       
     }
   }
-  int ohjausX=x;
-  if(x<0){
-   digitalWrite(M1,LOW);
-   ohjausX=-x;
-  } else {
-  digitalWrite(M1,HIGH); 
 
-  }  
-  
-  
-  int ohjausY=y;
-  if(y<0){
-   digitalWrite(M2,LOW);
-   ohjausY=-y;
-  } else {
-  digitalWrite(M2,HIGH); 
- 
-  }  
-             
-      analogWrite(E1, ohjausX);   //PWM Speed Control
-      analogWrite(E2, ohjausY); 
-
-       if(ultrasonic.RangeInCentimeters<10){
-        vaisto=100;
-      }
-      if(vaisto>0){
-        vaisto--;
-            if(vaisto>50){
-            goBack(); 
-            }
-            if(vaisto<50){
-              goLeft();
-            }
-      }
-  
-  delay(10);
-  /*
-  int i;
-  for(i = 0 ; i <= 1000; i+=5) 
-  { 
-    
-    if(i<250){
-       goStraight();
-       } else {
-         if(i<500){
-         goLeft();
-         } else {
-          if(i<750){
-          goRight();
-          }else{
-           goBack();
-          } 
-         }
-         delay(10);
+  ultrasonic.MeasureInCentimeters(); 
+  Serial.println(ultrasonic.RangeInCentimeters+1);
+  if(ultrasonic.RangeInCentimeters<20){
+    vaisto=50;
+  }
+  if(vaisto>0){
+    vaisto--;
+    Serial.print("vaisto: ");
+    Serial.print(vaisto);
+    if(vaisto>25){
+      goStraight(150); 
+      Serial.println(" Straight");
     }
-    
-  
-}  
-*/
+    if(vaisto<25){
+      Serial.println(" goLeft");
+      goLeft(150);
+    }
+  } 
+  else {
+    tempx=x;
+    tempy=y;
+    if(y>0){
+    digitalWrite(M1,HIGH);
+    digitalWrite(M2,HIGH);
+   
+  }
+  if(y<0){
+    digitalWrite(M1,LOW);
+    digitalWrite(M2,LOW);
+    tempy=-y;
+  }
+    if(tempx>=0){
+
+      ohjausL=tempy;
+      ohjausR=(1-tempx/127)*tempy;
+    }
+    if(tempx<0){
+      tempx=-tempx;
+      ohjausR=tempy;
+      ohjausL=(1-tempx/127)*tempy;
+    }
+
+    if(y==0){
+      analogWrite(E1, 0);   //PWM Speed Control
+      analogWrite(E2, 0);
+    }
+    else{
+      if(z & 1==1){
+      analogWrite(E1, ohjausL*2);   //PWM Speed Control
+      analogWrite(E2, ohjausR*2); 
+      } else {
+        analogWrite(E1, ohjaus);   //PWM Speed Control
+        analogWrite(E2, ohjaus); 
+    }
+    }
+  }
+
+  delay(10);
+
 }
+
+
+
